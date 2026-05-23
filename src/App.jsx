971a351b86458e8797a87561acdf20117214f16c
Lubeth <lubethjimenez@yahoo.com>
2026-05-23 15:50:21 -0400

    Add tracker Database, created JobDataIndex component w/ CSS to READ the data & display on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import JobDataIndex from './components/JobDataIndex'
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Capstone</h1>
+      <JobDataIndex />
     </>
   )
 }
---
+++ src/components/JobDataIndex.jsx
@@ -0,0 +1,49 @@
+
+import React, { useEffect, useState } from "react";
+import { supabase } from "../utils/supabase";
+import "./JobDataIndex.css";
+
+function JobDataIndex() {
+//READ from Database State//
+  const [jobs, setJobs] = useState([]);
+
+//READ//
+  async function fetchJobs() {
+    try {
+      const { data, error } = await supabase.from("tracker").select("*");
+      if (error) {
+        console.log(error);
+      } else {
+        //console.log(data);
+        setJobs(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+
+  return (
+    <div>
+      {/*READ from Database*/}
+      <h2 className="card-title">Job App Tracker:</h2>
+      {jobs.map((job) => (
+        <div className="index-card" key={job.id}>
+          <h3 style={{ textDecoration: "underline" }}>{job.company}</h3>
+          <p>Position: {job.position}</p>
+          <p>Status: {job.status}</p>
+          <p>Location: {job.location}</p>
+          <p>Salary: {job.salary}</p>
+          <p>Notes: {job.notes}</p>
+          <p>Applied Date: {job.applied_date}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default JobDataIndex;
